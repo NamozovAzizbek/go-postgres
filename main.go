b9cb2e670924be82ea95f9622ad2dc726cef0fdc
NamozovAzizbek <salom_007@proton.me>
2023-10-17 11:41:44 +0500

    stock

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/NamozovAzizbek/go-postgres/router"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Println("Starting server on the port 8000....")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
